27c8ac907e7b0b4d4e17ac68403ab9ef08af1f96
jkantner2 <kantnerj2@gmail.com>
2024-11-26 21:53:33 -0500

    add PLAYER_RADIUS to constants. Create circleshape and player files update main with player icon population

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -7,13 +8,21 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(0)
+
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+       
+        # limit framerate to 60 fps
+        dt = clock.tick(60) / 1000
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
